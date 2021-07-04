64ec8dc67fa285e1ff82e85b7ee371347e9b604e
Patel, Bhavin (Capita Software) <P10506572@capita.co.uk>
2021-07-04 12:54:41 +0530

    Add Participants, Professions controller.

---
+++ MeetupApis/Controllers/ProfessionsController.cs
@@ -0,0 +1,103 @@
+﻿using MeetupApis.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeetupApis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfessionsController : ControllerBase
+    {
+        private readonly APIDBContext _context;
+
+        public ProfessionsController(APIDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Professions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Profession>>> GetProfessions()
+        {
+            return await _context.Professions.ToListAsync();
+        }
+
+        // GET: api/Professions/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Profession>> GetProfession(int id)
+        {
+            var profession = await _context.Professions.FindAsync(id);
+
+            if (profession == null)
+            {
+                return NotFound();
+            }
+
+            return profession;
+        }
+
+        // PUT: api/Professions/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProfession(int id, Profession profession)
+        {
+            if (id != profession.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(profession).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProfessionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Professions
+        [HttpPost]
+        public async Task<ActionResult<Profession>> PostProfession(Profession profession)
+        {
+            _context.Professions.Add(profession);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProfession", new { id = profession.Id }, profession);
+        }
+
+        // DELETE: api/Professions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProfession(int id)
+        {
+            var profession = await _context.Professions.FindAsync(id);
+            if (profession == null)
+            {
+                return NotFound();
+            }
+
+            _context.Professions.Remove(profession);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProfessionExists(int id)
+        {
+            return _context.Professions.Any(e => e.Id == id);
+        }
+    }
+}
